d0c88a825e6e42b8d9cc741375cc16856a6c50d1
Alen Chery <achery03@yahoo.com>
2024-04-24 01:40:48 -0400

    Made changes to one folder

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,23 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import Budget from '../components/Budget';
 
+const Remaining = () => {
+   
+    const { expenses, budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    const totalExpenses = expenses.reduce((total,item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > newBudget ? 'alert-danger' : 'alert-success';
+    return (
+        <div className={'alert ${alertType}'} >
+            <span>Remaining: £{newBudget - totalExpenses}</span>
+            
+        </div>
+    );
+};
+export default Remaining;
